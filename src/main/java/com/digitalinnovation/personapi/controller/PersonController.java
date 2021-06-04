41c23ce6c35af856858e446deffc5be2e4dd3a8e
fpreviatti <fpreviatti@hotmail.com>
2021-06-04 01:52:39 -0300

    Criada entidades Person e Phone e relacionamento banco

--- src/main/java/com/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/personapi/controller/PersonController.java
@@ -1,6 +1,7 @@
 package com.digitalinnovation.personapi.controller;
 
 
+import com.digitalinnovation.personapi.entity.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,10 @@ public class PersonController {
 
     @GetMapping
     public String getBook(){
+
+        Person person = new Person();
+
+
         return "API Test!";
     }
 }
